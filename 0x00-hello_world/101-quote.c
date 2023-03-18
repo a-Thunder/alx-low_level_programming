ff7af22b18715fe2627683597d49275a72870d22
a-Thunder <atobi209@gmail.com>
2023-03-18 15:57:33 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - A program that prints and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
- * Return: 1
+ * Return: 1(Success)
  */
 
 int main(void)
