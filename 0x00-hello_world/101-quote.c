4bd32e4c0635c22b4ffbf23100777bccdc3221a4
a-Thunder <atobi209@gmail.com>
2023-03-18 16:00:03 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that prints and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ * main - A program that prints and that piece of art is useful"
  * Return: 1(Success)
  */
 
