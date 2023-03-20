aada05d2313bb14299cd3061e03ddd70bb671fa1
a-Thunder <atobi209@gmail.com>
2023-03-19 23:33:15 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,10 +4,11 @@
  * main - Prints the alphabet
  *
  * Return: 0 (Success)
+ *
  */
 int main(void)
 {
-	char alp[26] = "abdcdefghijklmnopqrstuvwxyz";
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
 	for (i = 0; i < 26; i++)
