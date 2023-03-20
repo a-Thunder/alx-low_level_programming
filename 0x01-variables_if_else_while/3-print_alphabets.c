8c34941548a11fccd970ad70b5ce069390b165ce
a-Thunder <atobi209@gmail.com>
2023-03-20 00:29:16 -0700

    prints the alphabet in lowercase and then uppercase, followed by a new line

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,12 +7,13 @@
  */
 int main(void)
 {
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
+
 	for (i = 0; i < 52; i++)
 	{
 		putchar(alp[i]);
 	}
 	putchar('\n');
-        return (0);
+	return (0);
 }
